9800e0f8337fd6a93e865c9ea983d9e6143d24d1
Ethan Henderson <ethan@zbee.codes>
2025-05-10 01:38:20 -0600

    Add WAR Hidden Features, for R6S and R7S

--- WrathCombo/Combos/CustomComboPreset.cs
+++ WrathCombo/Combos/CustomComboPreset.cs
@@ -6502,7 +6502,35 @@ SMN.JobID)]
 
     #endregion
 
-    // Last value = 18070
+    #region Hidden Features
+
+    [CustomComboInfo("Hidden Options", "Collection of cheeky or encounter-specific extra options only available to those in the know.\nDo not expect these options to be maintained, or even kept, after they are no longer Current.", WAR.JobID)]
+    [Hidden]
+    WAR_Hidden = 18071,
+
+    [ParentCombo(WAR_Hidden)]
+    [CustomComboInfo("R6S: Hold Burst on Squirrels", "When you're targeting Squirrels in R6S add phase, hold burst.\n(until about the time the first manta is dying)", WAR.JobID)]
+    [Hidden]
+    WAR_Hid_R6SHoldSquirrelBurst = 18072,
+
+    [ParentCombo(WAR_Hidden)]
+    [CustomComboInfo("R6S: Only Stun Jabberwock", "When in R6S, stun will only ever be used on the Jabberwock.", WAR.JobID)]
+    [Hidden]
+    WAR_Hid_R6SStunJabberOnly = 18073,
+
+    [ParentCombo(WAR_Hidden)]
+    [CustomComboInfo("R6S: Save Reprisal and Dark Missionary", "When in R6S, never try use Reprisal or Dark Missionaryautomatically.", WAR.JobID)]
+    [Hidden]
+    WAR_Hid_R6SNoAutoGroupMits = 18074,
+
+    [ParentCombo(WAR_Hidden)]
+    [CustomComboInfo("R7S: Only Interrupt the adds casting Circle AoEs", "When you're in R7S, Interrupting will only work when you're targeting an add casting the circle AoE.", WAR.JobID)]
+    [Hidden]
+    WAR_Hid_R7SCircleCastOnly = 18075,
+
+    #endregion
+
+    // Last value = 18075
 
     #endregion
 
--- WrathCombo/Combos/PvE/WAR/WAR.cs
+++ WrathCombo/Combos/PvE/WAR/WAR.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using Dalamud.Game.ClientState.Objects.Types;
 using WrathCombo.CustomComboNS;
 using WrathCombo.Data;
 namespace WrathCombo.Combos.PvE;
@@ -244,8 +245,12 @@ internal partial class WAR : Tank
                               JustUsed(Holmgang, 9f);
 
             // Interrupt
-            if (IsEnabled(CustomComboPreset.WAR_ST_Interrupt)
-                && Role.CanInterject())
+            if (IsEnabled(CustomComboPreset.WAR_ST_Interrupt) &&
+                HiddenFeaturesData.IsEnabledWith(
+                    CustomComboPreset.WAR_Hid_R7SCircleCastOnly,
+                    () => HiddenFeaturesData.Content.InR7S,
+                    () => HiddenFeaturesData.Targeting.R7SCircleCastingAdd) &&
+                Role.CanInterject())
                 return Role.Interject;
 
             #region Variant
@@ -614,11 +619,17 @@ internal partial class WAR : Tank
             if (actionID is not Overpower)
                 return actionID; //Our button
 
+            var r6SReady = !HiddenFeaturesData.IsEnabledWIth(
+                CustomComboPreset.WAR_Hid_R6SHoldSquirrelBurst,
+                () => HiddenFeaturesData.Targeting.R6SSquirrel &&
+                      CombatEngageDuration().TotalSeconds < 275);
+
+            bool savage = ContentCheck.IsInSavagePlusContent;
             bool justMitted = JustUsed(OriginalHook(RawIntuition), 4f) ||
-                              JustUsed(OriginalHook(Vengeance), 5f) ||
-                              JustUsed(ThrillOfBattle, 5f) ||
-                              JustUsed(Role.Rampart, 5f) ||
-                              JustUsed(Holmgang, 9f);
+                              JustUsed(OriginalHook(Vengeance), (savage ? 15f : 6f)) ||
+                              JustUsed(ThrillOfBattle, (savage ? 15f : 6f)) ||
+                              JustUsed(Role.Rampart, 6f) ||
+                              JustUsed(Holmgang, 7f);
 
             #region Stuns
 
@@ -629,7 +640,11 @@ internal partial class WAR : Tank
 
             // Stun
             if (IsEnabled(CustomComboPreset.WAR_AoE_Stun)
-                && Role.CanLowBlow())
+                && Role.CanLowBlow()
+                && HiddenFeaturesData.IsEnabledWith(
+                    CustomComboPreset.WAR_Hid_R6SStunJabberOnly,
+                    () => HiddenFeaturesData.Content.InR6S,
+                    () => HiddenFeaturesData.Targeting.R6SJabber))
                 return Role.LowBlow;
 
             #endregion
@@ -680,6 +695,9 @@ internal partial class WAR : Tank
 
                     //Reprisal
                     if (IsEnabled(CustomComboPreset.WAR_AoE_Advanced_Reprisal) && //Reprisal option is enabled
+                        HiddenFeaturesData.IsEnabledWIth(
+                            CustomComboPreset.WAR_Hid_R6SNoAutoGroupMits,
+                            () => !HiddenFeaturesData.Content.InR6S) &&
                         Role.CanReprisal(Config.WAR_AoE_Reprisal_Health, checkTargetForDebuff: false) && //Player's health is below selected threshold
                         (Config.WAR_AoE_Reprisal_SubOption == 0 || //Reprisal is enabled for all targets
                          TargetIsBoss() && Config.WAR_AoE_Reprisal_SubOption == 1)) //Reprisal is enabled for bosses only
@@ -693,6 +711,9 @@ internal partial class WAR : Tank
                 }
                 //Thrill
                 if (IsEnabled(CustomComboPreset.WAR_AoE_Advanced_Thrill) && //Thrill option is enabled
+                    HiddenFeaturesData.IsEnabledWIth(
+                        CustomComboPreset.WAR_Hid_R6SNoAutoGroupMits,
+                        () => !HiddenFeaturesData.Content.InR6S) &&
                     ActionReady(ThrillOfBattle) && //Thrill is ready
                     PlayerHealthPercentageHp() <= Config.WAR_AoE_Thrill_Health && //Player's health is below selected threshold
                     (Config.WAR_AoE_Thrill_SubOption == 0 || //Thrill is enabled for all targets
@@ -718,7 +739,7 @@ internal partial class WAR : Tank
 
             #endregion
 
-            if (CanWeave()) //in weave window
+            if (CanWeave() && r6SReady) //in weave window
             {
                 if (IsEnabled(CustomComboPreset.WAR_AoE_Advanced_Infuriate) && //Infuriate option is enabled
                     InCombat() && //in combat
@@ -736,7 +757,7 @@ internal partial class WAR : Tank
             }
 
             if (InCombat() && //in combat
-                HasStatusEffect(Buffs.SurgingTempest)) //has Surging Tempest
+                HasStatusEffect(Buffs.SurgingTempest) && r6SReady) //has Surging Tempest
             {
                 if (CanWeave()) //in weave window
                 {
