878f6d25cda11e234289fd0d6b8bb4b3d036bb6d
Ethan Henderson <ethan@zbee.codes>
2025-05-16 22:38:25 -0600

    Add `IGameObject` extensions that only return the object if it is in a certain classification

--- WrathCombo/Extensions/GameObjectExtensions.cs
+++ WrathCombo/Extensions/GameObjectExtensions.cs
@@ -2,7 +2,9 @@ using System;
 using System.Linq;
 using Dalamud.Game.ClientState.Objects.Types;
 using ECommons.DalamudServices;
+using ECommons.GameFunctions;
 using FFXIVClientStructs.FFXIV.Client.Game.Object;
+using WrathCombo.CustomComboNS.Functions;
 
 namespace WrathCombo.Extensions;
 
@@ -16,4 +18,23 @@ public static class GameObjectExtensions
     public static unsafe IGameObject? GetObjectFrom(GameObject* ptr) =>
         ptr == null ? null : Svc.Objects
             .FirstOrDefault(x => x.Address == (IntPtr)ptr);
+
+    #region Target Classification
+
+    public static IGameObject? IfFriendly (this IGameObject? obj) =>
+        obj != null && CustomComboFunctions.TargetIsFriendly(obj) ? obj : null;
+
+    public static IGameObject? IfHostile (this IGameObject? obj) =>
+        obj != null && obj.IsHostile() ? obj : null;
+
+    public static IGameObject? IfBoss (this IGameObject? obj) =>
+        obj != null && CustomComboFunctions.IsBoss(obj) ? obj : null;
+
+    public static IGameObject? IfQuestMob (this IGameObject? obj) =>
+        obj != null && CustomComboFunctions.IsQuestMob(obj) ? obj : null;
+
+    public static IGameObject? IfNeedsPositionals (this IGameObject? obj) =>
+        obj != null && CustomComboFunctions.TargetNeedsPositionals(obj) ? obj : null;
+
+    #endregion
 }
