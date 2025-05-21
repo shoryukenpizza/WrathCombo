d40bbeda27b81f0a033e05ed324e83f12d2f6716
Tartarga <109563717+Tartarga@users.noreply.github.com>
2025-05-21 07:39:57 -0500

    Removed Sloth Era BlockingStatuses checks, should be handled by ActionReady

--- WrathCombo/CustomCombo/Functions/Status.cs
+++ WrathCombo/CustomCombo/Functions/Status.cs
@@ -1,9 +1,5 @@
 ﻿using Dalamud.Game.ClientState.Objects.Types;
-using ECommons.DalamudServices;
 using FFXIVClientStructs.FFXIV.Client.Game;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using WrathCombo.Data;
 using WrathCombo.Services;
 using Status = Dalamud.Game.ClientState.Statuses.Status;
@@ -104,29 +100,14 @@ namespace WrathCombo.CustomComboNS.Functions
         /// <returns></returns>
         public static string GetStatusName(uint id) => StatusCache.GetStatusName(id);
 
-        /// <summary> Checks if the character has the Silence status. </summary>
-        /// <returns></returns>
-        public static bool HasSilence() => StatusCache.HasSilence();
-
-        /// <summary>
-        /// Checks if any entity has a Pacification status effect.
-        /// </summary>
-        /// <returns>True if a Pacification status is active; otherwise, false.</returns>
-        public static bool HasPacification() => StatusCache.HasPacification();
-
-        /// <summary> Checks if the character has the Amnesia status. </summary>
-        /// <returns></returns>
-        public static bool HasAmnesia() => StatusCache.HasAmnesia();
-
         public static bool TargetHasDamageDown(IGameObject? target) => StatusCache.HasDamageDown(target);
-
 
         public static bool TargetHasRezWeakness(IGameObject? target, bool checkForWeakness = true)
         {
-            if (checkForWeakness && StatusCache.HasWeakness(target))
+            if (checkForWeakness && HasStatusEffect(43, target, true)) //Weakness = 43
                 return true;
 
-            return StatusCache.HasBrinkOfDeath(target);
+            return HasStatusEffect(44, target, true); //Brink of Death = 44
         }
 
         /// <summary>
@@ -135,49 +116,6 @@ namespace WrathCombo.CustomComboNS.Functions
         /// <param name="target">The game object to check. Defaults to the current target if null.</param>
         /// <returns>True if the target has a cleansable debuff; otherwise, false.</returns>
         public static bool HasCleansableDebuff(IGameObject? target) => StatusCache.HasCleansableDebuff(target);
-        //{
-        //    try
-        //    {
-        //        return StatusCache.HasCleasableDebuff();
-        //        if (target is not IBattleChara chara || chara.StatusList == null)
-        //            return false;
-
-        //        foreach (var status in chara.StatusList)
-        //        {
-        //            if (status?.StatusId > 0 && status.RemainingTime > 0 && StatusCache.DispellableStatuses.Contains(status.StatusId))
-        //                return true;
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Svc.Log.Error(ex, $"Error checking for cleansable debuff on target {(target?.Name ?? "null")}");
-        //        return false;
-        //    }
-
-        //    return false;
-        //}
-
-        public static bool NoBlockingStatuses(uint actionId)
-        {
-            switch (ActionWatching.GetAttackType(actionId))
-            {
-                case ActionWatching.ActionAttackType.Weaponskill:
-                    if (HasPacification()) return false;
-                    return true;
-                case ActionWatching.ActionAttackType.Spell:
-                    if (HasSilence()) return false;
-                    return true;
-                case ActionWatching.ActionAttackType.Ability:
-                    if (HasAmnesia()) return false;
-                    return true;
-                case ActionWatching.ActionAttackType.Unknown:
-                    break;
-                default:
-                    break;
-            }
-
-            return true;
-        }
 
         /// <summary>
         /// Checks if the target is invincible due to status effects or encounter-specific mechanics.
@@ -185,38 +123,5 @@ namespace WrathCombo.CustomComboNS.Functions
         /// <param name="target">The game object to check.</param>
         /// <returns>True if the target is invincible; otherwise, false.</returns>
         public static bool TargetIsInvincible(IGameObject? target) => StatusCache.TargetIsInvincible(target);
-        //{
-        //    if (target is not IBattleChara tar || tar.StatusList == null)
-        //        return false;
-
-        //    var targetStatuses = tar.StatusList.Select(s => s.StatusId).ToHashSet();
-
-        //    // General invincibility check
-        //    if (StatusCache.HasInvincibleStatus(tar)) 
-        //        return true;
-        //    if (targetStatuses.Any(id => InvincibleStatuses.Contains(id)))
-        //        return true;
-
-
-        //    // Jeuno Ark Angel Encounter
-        //    if ((HasStatusEffect(4192) && !targetStatuses.Contains(4193)) ||
-        //        (HasStatusEffect(4194) && !targetStatuses.Contains(4195)) ||
-        //        (HasStatusEffect(4196) && !targetStatuses.Contains(4197)))
-        //        return true;
-
-        //    // YoRHa raid encounter
-        //    var alliance = GetAllianceGroup();
-        //    if ((alliance != AllianceGroup.GroupA && targetStatuses.Contains(2409)) ||
-        //        (alliance != AllianceGroup.GroupB && targetStatuses.Contains(2410)) ||
-        //        (alliance != AllianceGroup.GroupC && targetStatuses.Contains(2411)))
-        //        return true;
-
-        //    // Omega
-        //    if ((targetStatuses.Contains(1674) || targetStatuses.Contains(3454)) && (HasStatusEffect(1660) || HasStatusEffect(3499)) ||
-        //        (targetStatuses.Contains(1675) && (HasStatusEffect(1661) || HasStatusEffect(3500))))
-        //        return true;
-
-        //    return false;
-        //}
     }
 }
--- WrathCombo/Data/StatusCache.cs
+++ WrathCombo/Data/StatusCache.cs
@@ -1,13 +1,12 @@
 ﻿using Dalamud.Game.ClientState.Objects.Types;
+using Dalamud.Game.ClientState.Statuses;
 using ECommons.DalamudServices;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using static WrathCombo.CustomComboNS.Functions.CustomComboFunctions;
-//using Status = Dalamud.Game.ClientState.Statuses.Status; // conflicts with structs if not defined
-using Dalamud.Game.ClientState.Statuses;
-using ECommons;
+using Status = Dalamud.Game.ClientState.Statuses.Status; // conflicts with structs if not defined
 
 namespace WrathCombo.Data
 {
@@ -52,57 +51,14 @@ namespace WrathCombo.Data
         private static readonly Dictionary<uint, Lumina.Excel.Sheets.Status> StatusSheet = 
             Svc.Data.GetExcelSheet<Lumina.Excel.Sheets.Status>()!.ToDictionary(i => i.RowId, i => i);
 
-        /// <summary>
-        /// Enum defining status effects with their name IDs.
-        /// </summary>
-        public enum StatusEffect
-        {
-            Amnesia = 5,
-            Pacification = 6,
-            Silence = 7,
-            Weakness = 43,
-            BrinkOfDeath = 44,
-            DamageDown = 62
-        }
 
-        /// <summary>
-        /// Cached dictionary mapping status effects to their HashSet of status IDs.
-        /// </summary>
-        private static readonly Dictionary<StatusEffect, HashSet<uint>> StatusIdCaches = InitializeStatusIdCaches();
+        private static readonly HashSet<uint> DamageDownStatuses =
+            StatusSheet.TryGetValue(62, out var refRow)
+                ? [.. StatusSheet
+                .Where(x => x.Value.Name.ToString().Equals(refRow.Name.ToString(), StringComparison.CurrentCultureIgnoreCase))
+                .Select(x => x.Key)] : [];
 
-        /// <summary>
-        /// Initializes the status ID caches on startup for various ailments
-        /// Dictionary of Ailement / IDs (ie "Pacification" and all IDs for Pacification based on the localized name)
-        /// </summary>
-        private static Dictionary<StatusEffect, HashSet<uint>> InitializeStatusIdCaches()
-        {
-            var statusEffects = Enum.GetValues<StatusEffect>();
-            var nameToEffect = statusEffects
-                .Select(e => (Effect: e, Name: GetStatusName((uint)e)))
-                .Where(x => !string.IsNullOrEmpty(x.Name))
-                .ToDictionary(x => x.Name, x => x.Effect, StringComparer.OrdinalIgnoreCase);
-
-            var caches = statusEffects
-                .SelectMany(e => StatusSheet
-                    .Where(s => s.Value.Name.ToString().Equals(GetStatusName((uint)e), StringComparison.OrdinalIgnoreCase))
-                    .Select(s => (Effect: e, Id: s.Key)))
-                .GroupBy(x => x.Effect)
-                .ToDictionary(g => g.Key, g => g.Select(x => x.Id).ToHashSet());
-
-            return caches;
-        }
-
-        public static bool HasAmnesia() => HasStatusInCacheList(StatusEffect.Amnesia);
-
-        public static bool HasPacification() => HasStatusInCacheList(StatusEffect.Pacification);
-
-        public static bool HasSilence() => HasStatusInCacheList(StatusEffect.Silence);
-
-        public static bool HasWeakness(IGameObject? target) => HasStatusInCacheList(StatusEffect.Weakness, target);
-
-        public static bool HasBrinkOfDeath(IGameObject? target) => HasStatusInCacheList(StatusEffect.BrinkOfDeath, target);
-
-        public static bool HasDamageDown(IGameObject? target) => HasStatusInCacheList(StatusEffect.DamageDown, target);
+        public static bool HasDamageDown(IGameObject? target) => HasStatusInCacheList(DamageDownStatuses, target);
 
         /// <summary>
         /// A cached set of dispellable status IDs for quick lookup.
@@ -111,6 +67,7 @@ namespace WrathCombo.Data
             StatusSheet
                 .Where(kvp => kvp.Value.CanDispel)
                 .Select(kvp => kvp.Key)];
+
         public static bool HasCleansableDebuff(IGameObject? target) => HasStatusInCacheList(DispellableStatuses, target);
 
         /// <summary>
@@ -124,11 +81,9 @@ namespace WrathCombo.Data
         private static HashSet<uint> GenerateInv()
         {
             //Search by Invincibility Icon
-            uint targetIcon = StatusSheet.FirstOrDefault(row => row.Value.RowId == 325).Value.Icon;
-            //uint targetIcon = Svc.Data.GetExcelSheet<Lumina.Excel.Sheets.Status>().FirstOrDefault(row => row.RowId == 325).Icon;
-            //return [.. Svc.Data.GetExcelSheet<Lumina.Excel.Sheets.Status>()
-            //    .Where(kvp => kvp.Icon == targetIcon)
-            //    .Select(kvp => kvp.RowId)];
+            uint targetIcon = 215024; //StatusSheet.FirstOrDefault(row => row.Value.RowId == 325).Value.Icon;
+            //Svc.Log.Debug($"Invincible Icon: {targetIcon}");
+
             var invincibles = StatusSheet.Where(row => row.Value.Icon == targetIcon).Select(row => row.Key).ToHashSet();
             //Add Random Invulnerabilities not yet assigned to TerritoryType
             invincibles.UnionWith([151, 198, 385, 469, 592, 1240, 1302, 1303, 1567, 1936, 2413, 2654, 3012, 3039, 3052, 3054, 4175]);
@@ -195,9 +150,9 @@ namespace WrathCombo.Data
                     if ((tar.StatusList.Any(x => x.StatusId == 3454) && HasStatusEffect(3499)) ||
                         (tar.StatusList.Any(x => x.StatusId == 1675) && HasStatusEffect(3500)))
                         return true;
-                    
+
                     //Check for any ol invincibility
-                    if (targetStatuses.Any(id => InvincibleStatuses.Contains(id))) return true;
+                    if (CompareLists(InvincibleStatuses, targetStatuses)) return true;
 
                     return false;
                 default:
@@ -206,10 +161,15 @@ namespace WrathCombo.Data
 
             // General invincibility check
             // Due to large size of InvincibleStatuses, best to check process this way
-            if (targetStatuses.Any(id => InvincibleStatuses.Contains(id))) return true;
+            if (CompareLists(InvincibleStatuses, targetStatuses)) return true;
             return false;
         }
 
+        /// <summary>
+        /// Looks up the name of a Status by ID in Lumina Sheets
+        /// </summary>
+        /// <param name="id">Status ID</param>
+        /// <returns></returns>
         public static string GetStatusName(uint id) => StatusSheet.TryGetValue(id, out var status) ? status.Name.ToString() : "Unknown Status";
 
         /// <summary>
@@ -229,33 +189,30 @@ namespace WrathCombo.Data
         }
 
         /// <summary>
-        /// Compares a list of Status IDs against the Player or optional Target.
+        /// Checks a GameObject's Status list against a set of Status IDs
         /// </summary>
-        /// <param name="list">HashSet uint. Use list from StatusCache</param>
-        /// <param name="target">Optional IGameObject to check</param>
-        /// 
+        /// <param name="statusList">Hashset of Status IDs to check</param>
+        /// <param name="gameObject">GameObject to check</param>
         /// <returns></returns>
-        internal static bool HasStatusInCacheList(HashSet<uint> list, IGameObject? target)
+        internal static bool HasStatusInCacheList(HashSet<uint> statusList, IGameObject? gameObject = null)
         {
-            if (list.Count is not 0)
+            if (gameObject is not IBattleChara chara || chara.StatusList == null)
+                return false;
+            var targetStatuses = chara.StatusList.Select(s => s.StatusId).ToHashSet();
+            return statusList.Count switch
             {
-                if (target is not IBattleChara chara || chara.StatusList == null)
-                    return false;
+                0 => false,
+                _ => CompareLists(statusList, targetStatuses)
+            };
+        }
 
-                foreach (uint status in list)
-                {
-                    if (HasStatusEffect(status, chara, true)) return true;
-                }
-            }
-            return false;
-        }
-        
         /// <summary>
-        /// Checks if a target has any status effect for the given status effect.
+        /// Compares two hashsets, in this case, used to compare a cached set of status IDs against a character's StatusID list
         /// </summary>
-        internal static bool HasStatusInCacheList(StatusEffect effect, IGameObject? target = null)
-        {
-            return HasStatusInCacheList(StatusIdCaches[effect], target);
-        }
+        /// <param name="statusList"></param>
+        /// <param name="charaStatusList"></param>
+        /// <returns></returns>
+        private static bool CompareLists(HashSet<uint> statusList, HashSet<uint> charaStatusList) => 
+            charaStatusList.Any(id => statusList.Contains(id));
     }
 }
