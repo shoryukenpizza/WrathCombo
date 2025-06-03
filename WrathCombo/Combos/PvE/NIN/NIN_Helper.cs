db7d4a1a76ef24d751b39e2ddfd85fc0e4fc286c
Taurenkey <github@taurenkey.co.uk>
2025-06-03 07:12:22 +0100

    Crank that shit up

--- WrathCombo/Combos/PvE/NIN/NIN_Helper.cs
+++ WrathCombo/Combos/PvE/NIN/NIN_Helper.cs
@@ -567,7 +567,7 @@ internal partial class NIN
 
         public bool ContinueCurrentMudra(ref uint actionID)
         {
-            if (ActionWatching.TimeSinceLastAction.TotalSeconds >= 0.5 && CurrentNinjutsu == Ninjutsu)
+            if (ActionWatching.TimeSinceLastAction.TotalSeconds >= 1 && CurrentNinjutsu == Ninjutsu)
             {
                 InMudra = false;
                 ActionWatching.LastAction = 0;
