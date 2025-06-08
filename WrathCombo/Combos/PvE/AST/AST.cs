8cbbcd7cb61cfed047d3848f25b3d3987c4f630c
edewen <edewen@gmail.com>
2025-06-08 14:43:56 -0400

    Status Cap Code

--- WrathCombo/Combos/PvE/AST/AST.cs
+++ WrathCombo/Combos/PvE/AST/AST.cs
@@ -152,7 +152,8 @@ internal partial class AST : Healer
                         float refreshTimer = Config.AST_ST_DPS_CombustUptime_Threshold;
                         int hpThreshold = Config.AST_ST_DPS_CombustSubOption == 1 || !InBossEncounter() ? Config.AST_DPS_CombustOption : 0;
                         if (GetStatusEffectRemainingTime(dotDebuffID, CurrentTarget) <= refreshTimer &&
-                            GetTargetHPPercent() > hpThreshold)
+                            GetTargetHPPercent() > hpThreshold &&
+                            CanApplyStatus(CurrentTarget,dotDebuffID))
                             return OriginalHook(Combust);
 
                         //Alternate Mode (idles as Malefic)
