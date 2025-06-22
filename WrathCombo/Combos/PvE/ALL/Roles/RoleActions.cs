6decde58fbd1e234dd75bf2f6dddea04a2583f79
Ethan Henderson <ethan@zbee.codes>
2025-06-22 10:30:04 -0600

    fix(Tanks): `Low Blow` now checks for diminishing returns

--- WrathCombo/Combos/PvE/ALL/Roles/RoleActions.cs
+++ WrathCombo/Combos/PvE/ALL/Roles/RoleActions.cs
@@ -1,4 +1,5 @@
-﻿using static WrathCombo.CustomComboNS.Functions.CustomComboFunctions;
+﻿using WrathCombo.Data;
+using static WrathCombo.CustomComboNS.Functions.CustomComboFunctions;
 
 namespace WrathCombo.Combos.PvE;
 
@@ -181,7 +182,7 @@ internal static partial class RoleActions
             ActionReady(Rampart) && PlayerHealthPercentageHp() <= healthPercent;
 
         public static bool CanLowBlow() =>
-            ActionReady(LowBlow) && TargetIsCasting();
+            ActionReady(LowBlow) && TargetIsCasting() && (ICDTracker.StatusIsExpired(All.Debuffs.Stun, CurrentTarget.GameObjectId) || ICDTracker.NumberOfTimesApplied(All.Debuffs.Stun, CurrentTarget.GameObjectId) < 3);
 
         public static bool CanProvoke() =>
             ActionReady(Provoke);
