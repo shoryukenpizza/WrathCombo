dbbab875bf4bf8cd3068b8110d2a813bdb7f3c9b
nativepapaya <native@sloth>
2025-07-03 11:05:36 -0400

    Triplecast leyline option added

--- WrathCombo/Combos/PvE/BLM/BLM.cs
+++ WrathCombo/Combos/PvE/BLM/BLM.cs
@@ -223,7 +223,9 @@ internal partial class BLM : Caster
 
                     if (IsEnabled(CustomComboPreset.BLM_ST_Triplecast) &&
                         ActionReady(Triplecast) && IsOnCooldown(Role.Swiftcast) &&
-                        !HasStatusEffect(Role.Buffs.Swiftcast) && !HasStatusEffect(Buffs.Triplecast) && !HasStatusEffect(Buffs.LeyLines) &&
+                        !HasStatusEffect(Role.Buffs.Swiftcast) && !HasStatusEffect(Buffs.Triplecast) && 
+                        HasStatusEffect(Buffs.LeyLines) && IsNotEnabled(CustomComboPreset.BLM_ST_Triplecast_Leyline) || 
+                        !HasStatusEffect(Buffs.LeyLines) &&
                         ((BLM_ST_MovementOption[0] && GetRemainingCharges(Triplecast) > BLM_ST_Triplecast_Movement) ||
                          !BLM_ST_MovementOption[0]) && JustUsed(Despair) && !ActionReady(Manafont))
                         return Triplecast;
