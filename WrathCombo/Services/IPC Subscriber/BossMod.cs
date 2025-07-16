582a5ccf0a8e28b3e826322b00890ef7b0ad1817
Ethan Henderson <ethan@zbee.codes>
2025-07-16 14:11:35 -0600

    feat(BossModIPC): +Documentation

--- WrathCombo/Services/IPC Subscriber/BossMod.cs
+++ WrathCombo/Services/IPC Subscriber/BossMod.cs
@@ -9,20 +9,46 @@ namespace WrathCombo.Services.IPC_Subscriber;
 
 internal static class BossModIPC
 {
+    /// <summary>
+    ///     Initiate the IPC, and save the tokens required for disposal.
+    /// </summary>
     private static EzIPCDisposalToken[] _disposalTokens =
-        EzIPC.Init(typeof(BossModIPC), "BossMod", SafeWrapper.None);
+        EzIPC.Init(typeof(BossModIPC), "BossMod", SafeWrapper.IPCException);
 
+    #region Version Checking
+
+    /// <summary>
+    ///     Checks that either a debug build of BossMod is present, or a release of
+    ///     sufficient version.
+    /// </summary>
     internal static bool IsEnabled =>
         InstalledVersion >= ValidVersion || // release version
         InstalledVersion == new Version(0, 0, 0, 0); // debug version
     
+    /// <summary>
+    ///     Checks the installed version of the BossMod plugin.
+    /// </summary>
+    /// <remarks>
+    ///     If not found, will give <c>0.0.0.1</c> as the version, as
+    ///     <c>0.0.0.0</c> is used for debug builds.
+    /// </remarks>
     internal static Version InstalledVersion =>
         (DalamudReflector.TryGetDalamudPlugin("BossMod", out var dalamudPlugin, false, true)
             ? dalamudPlugin.GetType().Assembly.GetName().Version!
             : new Version(0, 0, 0, 1)); // no version found
     
+    /// The version where the IPC endpoints were introduced.
     private static readonly Version ValidVersion = new(0,3,0,6);
+
+    #endregion
     
+    /// <summary>
+    ///     Wrapper for <see cref="HasEntries"/> IPC call, only checking if the
+    ///     IPC is enabled.
+    /// </summary>
+    /// <returns>
+    ///     Whether the action queue has any automatic entries queued.
+    /// </returns>
     internal static bool HasAutomaticActionsQueued()
     {
         if (!IsEnabled)
