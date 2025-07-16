878bf09577ccd5e82dd564abe86f97e981a7d49a
Ethan Henderson <ethan@zbee.codes>
2025-07-16 02:53:34 -0600

    feat(BossModIPC): +Implementation
    Adds subscription to the BossMod IPC, at least as far as it is needed for `ConflictingPlugins`.

--- WrathCombo/Services/IPC Subscriber/Bossmod.cs
+++ WrathCombo/Services/IPC Subscriber/Bossmod.cs
@@ -1,6 +1,43 @@
-﻿namespace WrathCombo.Services.IPC_Subscriber;
+﻿using System;
+using System.Collections.Generic;
+using ECommons;
+using ECommons.EzIpcManager;
+using ECommons.Logging;
+using ECommons.Reflection;
 
-public class Bossmod
+namespace WrathCombo.Services.IPC_Subscriber;
+
+internal static class BossmodIPC
 {
+    private static EzIPCDisposalToken[] _disposalTokens =
+        EzIPC.Init(typeof(BossmodIPC), "BossMod", SafeWrapper.None);
+
+    internal static bool IsEnabled => InstalledVersion >= ValidVersion;
+    internal static Version InstalledVersion =>
+        (DalamudReflector.TryGetDalamudPlugin("BossMod", out var dalamudPlugin, false, true)
+            ? dalamudPlugin.GetType().Assembly.GetName().Version!
+            : new Version(0, 0, 0, 0));
+    private static readonly Version ValidVersion = new(0,3,0,6);
     
+    internal static bool HasAutomaticActionsQueued(int max = 0)
+    {
+        var entries = NonManualEntries().Count;
+        PluginLog.Verbose($"[ConflictingPlugins] [Bossmod] " +
+                          $"`HasAutomaticActionsQueued` {entries} automatic entries queued.");
+        return IsEnabled && entries > max;
+    }
+    
+#pragma warning disable CS0649, CS8618 // EzIPC complaints
+    [EzIPC("Configuration.%m")]
+    internal static readonly Func<DateTime> LastModified;
+    [EzIPC("Rotation.ActionQueue.%m")]
+    private static readonly Func<List<object>> NonManualEntries;
+#pragma warning restore CS8618, CS0649
+
+    internal static void Dispose()
+    {
+        foreach (var token in _disposalTokens)
+            try { token.Dispose(); }
+            catch (Exception ex) { ex.Log(); }
+    }
 }
