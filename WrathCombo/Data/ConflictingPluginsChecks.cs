1c61b349e29cb3b0b4d71775d6bd9ad679421b3e
Ethan Henderson <ethan@zbee.codes>
2025-07-19 01:00:50 -0600

    feat(Conflicts): +`ConflictCheck` abstract, +`MOAction` checking
    This adds the `ConflictCheck` abstract class for all plugin conflict checking classes to inherit, that reduces some of the duplicate code required between those classes and helps enforce standard names for public Properties and consistent logging.
    This also fits the `BossModCheck` class to that new inheritance.
    This also adds the initial `MOAction` conflict checking (waiting on an attribute for the CCP to list out which actions are retargeted, specifically).

--- WrathCombo/Data/ConflictingPluginsChecks.cs
+++ WrathCombo/Data/ConflictingPluginsChecks.cs
@@ -1,107 +1,39 @@
-﻿using System;
+﻿#region
+
+using System;
 using Dalamud.Game.ClientState.Conditions;
 using ECommons.DalamudServices;
 using ECommons.Logging;
+using WrathCombo.Combos.PvE;
 using WrathCombo.Services.IPC_Subscriber;
 using EZ = ECommons.Throttlers.EzThrottler;
 using TS = System.TimeSpan;
+
+#endregion
 
 namespace WrathCombo.Data;
 
 public static class ConflictingPluginsChecks
 {
     private static bool _cancelConflictChecks;
-    
-    internal static BossModCheck BossMod { get; } = new();
-    internal static BossModCheck BossModReborn { get; } = new(true);
-
-    public class BossModCheck
-    {
-        private BossModIPC IPC;
-        private string Name => IPC.PluginName;
-        
-        public bool Conflicted;
-        private DateTime? _conflictRegistered;
-        private DateTime? _conflictFirstSeen;
-        private int _conflictsInARow;
-        private int _maxConflictsInARow = 4;
-        
-        public BossModCheck(bool reborn = false)
-        {
-            IPC = !reborn
-                ? new BossModIPC()
-                : new BossModIPC("BossModReborn", new Version(7, 2, 5, 87));
-            PluginLog.Verbose($"[ConflictingPlugins] [{Name}] Set up for Checking");
-        }
-
-        public void CheckForConflict()
-        {
-            // Throttle the BossMod check
-            if (!EZ.Throttle($"conflictCheck{Name}", TS.FromSeconds(8)) ||
-                !Svc.Condition[ConditionFlag.InCombat])
-                return;
-            
-            PluginLog.Verbose($"[ConflictingPlugins] [{Name}] Performing Check ...");
-            
-#if DEBUG
-            _maxConflictsInARow = 1;
-#endif
-
-            // Reset the conflict timer, must exceed the threshold within 2 minutes
-            if (_conflictFirstSeen is not null &&
-                _conflictFirstSeen - DateTime.Now > TS.FromMinutes(2))
-            {
-                PluginLog.Verbose($"[ConflictingPlugins] [{Name}] Resetting Conflict Check");
-                _conflictFirstSeen = null;
-                _conflictsInARow = 0;
-            }
-
-            // Clear the conflict
-            if (!IPC.IsEnabled || // disabled
-                (_conflictRegistered is not null && // there is a conflict currently
-                 IPC.LastModified() > _conflictRegistered)) // bm config changed
-            {
-                PluginLog.Verbose($"[ConflictingPlugins] [{Name}] IPC not enabled, or config updated");
-                Conflicted = false;
-                _conflictsInARow = 0;
-                return;
-            }
-
-            // Add a conflict note
-            if (IPC.HasAutomaticActionsQueued())
-            {
-                PluginLog.Verbose($"[ConflictingPlugins] [{Name}] Actions are Queued");
-                _conflictFirstSeen ??= DateTime.Now; // Set the time seen if empty
-                _conflictsInARow++;
-            }
-
-            // Save a complete conflict
-            if (_conflictsInARow > _maxConflictsInARow)
-            {
-                PluginLog.Debug($"[ConflictingPlugins] [{Name}] Marked Conflict!");
-                Conflicted = true;
-                _conflictRegistered = DateTime.Now;
-            }
-        }
-
-        // ReSharper disable once InconsistentNaming
-        public void dispose()
-        {
-            IPC.Dispose();
-        }
-    }
 
     private static readonly Action RunChecks = () =>
     {
         if (_cancelConflictChecks)
             return;
 
-        PluginLog.Verbose($"[ConflictingPlugins] Periodic check for conflicting plugins");
+        PluginLog.Verbose(
+            "[ConflictingPlugins] Periodic check for conflicting plugins");
         BossMod.CheckForConflict();
         BossModReborn.CheckForConflict();
+        MOAction.CheckForConflict();
 
         Svc.Framework.RunOnTick(RunChecks!, TS.FromSeconds(4.11));
     };
+
+    internal static BossModCheck BossMod { get; } = new();
+    internal static BossModCheck BossModReborn { get; } = new(true);
+    internal static MOActionCheck MOAction { get; } = new();
 
     public static void Begin()
     {
@@ -114,9 +46,122 @@ public static class ConflictingPluginsChecks
         Svc.Framework.RunOnTick(RunChecks, ts);
     }
 
-    public static void Dispose() {
-        BossMod.dispose();
-        BossModReborn.dispose();
+    public static void Dispose()
+    {
+        BossMod.Dispose();
+        BossModReborn.Dispose();
+        MOAction.Dispose();
         _cancelConflictChecks = true;
+    }
+
+    internal sealed class BossModCheck(bool reborn = false)
+        : ConflictCheck(reborn
+            ? new BossModIPC()
+            : new BossModIPC("BossModReborn", new Version(7, 2, 5, 87)))
+    {
+        private DateTime? _conflictFirstSeen;
+        private DateTime? _conflictRegistered;
+        private int _conflictsInARow;
+        private int _maxConflictsInARow = 4;
+
+        protected override BossModIPC IPC => (BossModIPC)_ipc;
+
+        public override void CheckForConflict()
+        {
+            // Throttle the check
+            if (!EZ.Throttle($"conflictCheck{Name}", TS.FromSeconds(8)) ||
+                !Svc.Condition[ConditionFlag.InCombat])
+                return;
+
+            PluginLog.Verbose($"[ConflictingPlugins] [{Name}] Performing Check ...");
+
+#if DEBUG
+            _maxConflictsInARow = 1;
+#endif
+
+            // Reset the conflict timer, must exceed the threshold within 2 minutes
+            if (_conflictFirstSeen is not null &&
+                _conflictFirstSeen - DateTime.Now > TS.FromMinutes(2))
+            {
+                PluginLog.Verbose(
+                    $"[ConflictingPlugins] [{Name}] Resetting Conflict Check");
+                _conflictFirstSeen = null;
+                _conflictsInARow = 0;
+            }
+
+            // Clear the conflict
+            if (!IPC.IsEnabled || // disabled
+                (_conflictRegistered is not null && // there is a conflict marked
+                 IPC.LastModified() > _conflictRegistered)) // bm config changed
+            {
+                PluginLog.Verbose(
+                    $"[ConflictingPlugins] [{Name}] IPC not enabled, or config updated");
+                Conflicted = false;
+                _conflictsInARow = 0;
+                return;
+            }
+
+            // Add a conflict note
+            if (IPC.HasAutomaticActionsQueued())
+            {
+                PluginLog.Verbose(
+                    $"[ConflictingPlugins] [{Name}] Actions are Queued");
+                _conflictFirstSeen ??= DateTime.Now; // Set the time seen if empty
+                _conflictsInARow++;
+            }
+
+            // Save a complete conflict
+            if (_conflictsInARow > _maxConflictsInARow)
+            {
+                PluginLog.Debug($"[ConflictingPlugins] [{Name}] Marked Conflict!");
+                Conflicted = true;
+                _conflictRegistered = DateTime.Now;
+            }
+        }
+    }
+
+    internal sealed class MOActionCheck() : ConflictCheck(new MOAction())
+    {
+        protected override MOAction IPC => (MOAction)_ipc;
+
+        public override void CheckForConflict()
+        {
+            // Throttle the check
+            if (!EZ.Throttle($"conflictCheck{Name}", TS.FromSeconds(20)))
+                return;
+            
+            PluginLog.Verbose($"[ConflictingPlugins] [{Name}] Performing Check ...");
+
+            var retargetedActions = IPC.GetRetargetedActions();
+            if (retargetedActions.Contains(DRK.BlackestNight))
+                Conflicted = true;
+        }
+    }
+
+    internal abstract class ConflictCheck : IDisposable
+    {
+        // ReSharper disable once InconsistentNaming
+        protected readonly ReusableIPC _ipc;
+
+        protected ConflictCheck(ReusableIPC ipc)
+        {
+            _ipc = ipc;
+            if (_ipc.IsEnabled)
+                PluginLog.Verbose(
+                    $"[ConflictingPlugins] [{Name}] Setup for Checking " +
+                    $"(v{ipc.InstalledVersion})");
+        }
+
+        // ReSharper disable once UnusedMemberInSuper
+        protected abstract ReusableIPC IPC { get; }
+
+        public bool Conflicted { get; protected set; }
+
+        protected string Name => _ipc.PluginName;
+
+        // ReSharper disable once MemberHidesStaticFromOuterClass
+        public virtual void Dispose() => _ipc.Dispose();
+
+        public abstract void CheckForConflict();
     }
 }
