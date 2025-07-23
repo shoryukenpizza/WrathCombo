cd8d600633176819ac131e95fbe11c46db5e0538
Ethan Henderson <ethan@zbee.codes>
2025-07-22 18:18:28 -0600

    feat(RedirectIPC): +Reflection Implementation

--- WrathCombo/Services/IPC_Subscriber/Redirect.cs
+++ WrathCombo/Services/IPC_Subscriber/Redirect.cs
@@ -1,5 +1,81 @@
-﻿namespace WrathCombo.Services.IPC_Subscriber;
+﻿#region
 
-public class Redirect
+using System;
+using System.Collections;
+using System.Linq;
+using ECommons;
+using ECommons.Logging;
+using ECommons.Reflection;
+using WrathCombo.Extensions;
+
+#endregion
+
+namespace WrathCombo.Services.IPC_Subscriber;
+
+internal sealed class Redirect(
+    string? pluginName = null,
+    Version? validVersion = null)
+    : ReusableIPC(pluginName ?? "Redirect",
+        validVersion ?? new Version(1, 2, 5, 0),
+        true)
 {
+    private object Configuration =>
+        Plugin.GetFoP("Configuration");
+
+    private bool AutoMouseoverBeneficial =>
+        Configuration.GetFoP<bool>("DefaultMouseoverFriendly");
+
+    private bool AutoMouseoverGround =>
+        Configuration.GetFoP<bool>("DefaultMouseoverGround");
+
+    private uint[] Redirections =>
+        (Configuration.GetFoP("Redirections") as IDictionary)?
+        .Keys.Cast<uint>().ToArray() ?? [];
+
+    public uint[] GetRetargetedActions()
+    {
+        if (!IsEnabled)
+        {
+            PluginLog.Debug($"[ConflictingPlugins] [{PluginName}] is not enabled.");
+            return [];
+        }
+
+        try
+        {
+            var actions = Redirections;
+            PluginLog.Verbose(
+                $"[ConflictingPlugins] [{PluginName}] Has `Redirections`: " +
+                (actions.Length > 0));
+            foreach (var action in actions)
+            {
+                PluginLog.Verbose(
+                    $"[ConflictingPlugins] [{PluginName}] Action: {action.ActionName()}");
+            }
+            return actions;
+        }
+        catch (Exception e)
+        {
+            PluginLog.Warning($"[ConflictingPlugins] [{PluginName}] " +
+                              $"Getting `Redirections` failed: {e.ToStringFull()}");
+            return [];
+        }
+    }
+    
+    public bool AreGroundTargetedActionsRedirected()
+    {
+        PluginLog.Verbose(
+            $"[ConflictingPlugins] [{PluginName}] " +
+            $"Has `AutoMouseoverGround`: {AutoMouseoverGround}");
+        
+        return AutoMouseoverGround;
+    }
+
+    public bool AreBeneficialActionsRedirected()
+    {
+        PluginLog.Verbose(
+            $"[ConflictingPlugins] [{PluginName}] " +
+            $"Has `AutoMouseoverBeneficial`: {AutoMouseoverBeneficial}");
+        
+        return AutoMouseoverBeneficial;
+    }
 }
